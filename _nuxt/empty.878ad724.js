23c4be26bb4f8ecc01fe5de30281c891b999d393
fsoel <120105124+fsoel@users.noreply.github.com>
2023-11-12 20:05:45 +0000

    Deploying to gh-pages from @ lmg-anrath/workadventure-website@e88ccab80694f09956e689416626fdfd534ee334 🚀

---
+++ _nuxt/empty.878ad724.js
@@ -0,0 +1,1 @@
+import{_ as t,c as o,C as r,o as c}from"./entry.d0ed7c91.js";const n={};function s(e,a){return c(),o("div",null,[r(e.$slots,"default")])}const _=t(n,[["render",s]]);export{_ as default};
